e8f004217d6df6841771552133ba78c0e3cc6a25
Shelly-cloud <62568344+Shelly-cloud@users.noreply.github.com>
2022-08-29 03:51:58 +0530

    Changed config
    Change database credentials in config file.

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST', '10.0.12.70');
-define('DB_USER', 'pbadmin_uat');
-define('DB_PASSWORD', 'PBuat@123');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'git');
 define('TABLE_PREFIX', '');
 define('GITHUB_API_HOST', 'https://api.github.com');
